66da746e373dbe47f5f415b6279a4ef460b96ff5
Jose-PC <Jose@PC>
2015-02-20 19:28:32 +0100

    reparat 25

--- src/com/example/calculadora/MainActivity.java
+++ src/com/example/calculadora/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends ActionBarActivity implements OnClickListener  {
 	
